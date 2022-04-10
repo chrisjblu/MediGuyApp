a94c8c6e79b8bb629d4aceb0544c9975b007c208
chrisjblu <c.angbalu@gmail.com>
2022-04-10 16:00:18 +0100

    Covid Tracker Feature Complete

--- app/src/main/java/com/example/mediguyapp/SettingsActivity.java
+++ app/src/main/java/com/example/mediguyapp/SettingsActivity.java
@@ -6,18 +6,86 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.mediguyapp.api.ApiUtilities;
+import com.example.mediguyapp.api.CountryData;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+
+import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SettingsActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
 
+    private TextView totalConfirm, totalActive, totalRecovered, totalDeath, totalTests;
+    private TextView todayConfirm, todayRecovered, todayDeath, coviddate;
+
+
+    private List<CountryData> list;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        list = new ArrayList<>();
+
+        init();
+
+        ApiUtilities.getApiInterface().getCountryData()
+                .enqueue(new Callback<List<CountryData>>() {
+                    @Override
+                    public void onResponse(Call<List<CountryData>> call, Response<List<CountryData>> response) {
+                        list.addAll(response.body());
+
+                        for(int i = 0; i<list.size(); i++){
+                            if(list.get(i).getCountry().equals("Ireland")){
+                                int confirm = Integer.parseInt(list.get(i).getCases());
+                                int active = Integer.parseInt(list.get(i).getActive());
+                                int recovered = Integer.parseInt(list.get(i).getRecovered());
+                                int death = Integer.parseInt(list.get(i).getDeaths());
+
+                                totalConfirm.setText(NumberFormat.getInstance().format(confirm));
+                                totalActive.setText(NumberFormat.getInstance().format(active));
+                                totalRecovered.setText(NumberFormat.getInstance().format(recovered));
+                                totalDeath.setText(NumberFormat.getInstance().format(death));
+
+                                todayDeath.setText(NumberFormat.getInstance().format(Integer.parseInt(list.get(i).getTodayDeaths())));
+                                todayConfirm.setText(NumberFormat.getInstance().format(Integer.parseInt(list.get(i).getTodayCases())));
+                                todayRecovered.setText(NumberFormat.getInstance().format(Integer.parseInt(list.get(i).getTodayRecovered())));
+                                totalTests.setText(NumberFormat.getInstance().format(Integer.parseInt(list.get(i).getTests())));
+
+
+                                setText(list.get(i).getUpdated());
+
+
+
+
+
+
+
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<CountryData>> call, Throwable t) {
+                        Toast.makeText(SettingsActivity.this, "Error : "+t.getMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                });
 
 
         bottomNavigationView = findViewById(R.id.bottom_navigator);
@@ -49,5 +117,33 @@ public class SettingsActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    private void setText(String updated) {
+        DateFormat format = new SimpleDateFormat("MMM dd, yyyy");
+
+        long milliseconds = Long.parseLong(updated);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(milliseconds);
+
+        coviddate.setText("Updated at " +format.format(calendar.getTime()));
+    }
+
+    private void init(){
+        totalConfirm = findViewById(R.id.totalConfirm);
+        totalActive = findViewById(R.id.totalActive);
+        totalRecovered = findViewById(R.id.totalRecovered);
+        totalDeath = findViewById(R.id.totalDeath);
+        totalTests = findViewById(R.id.totalTests);
+        todayConfirm = findViewById(R.id.todayConfirm);
+        todayRecovered = findViewById(R.id.todayRecovered);
+        todayDeath = findViewById(R.id.todayDeath);
+        coviddate = findViewById(R.id.coviddateTV);
+
+
+
+
+
     }
 }
